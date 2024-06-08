24e12b3c53b8ca8bfc08b8bf803fb2df65018aa1
Furkan Genç <gencfurkan2001@gmail.com>
2024-06-08 18:29:21 +0300

    Add setup

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
+# SETUP
+conda create -n custom_loss python=3.9
+conda activate custom_loss
+conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
+pip install -r requirements.txt
+bash download_data.sh
 
+# USAGE
 
 # Train a model with CrossEntropyLoss on CIFAR10 dataset for 2 epochs to initialize the model with CustomLoss
 python main.py --loss CrossEntropyLoss --save_path ./results/CIFAR10/exp1/ --dataset CIFAR10 --max_num_epochs 200
@@ -10,4 +17,4 @@ python main.py --loss CustomLoss --save_path ./results/CIFAR10/exp2/ --model_pat
 python main.py --loss CrossEntropyLoss --save_path ./results/CIFAR100/exp1/ --dataset CIFAR100 --max_num_epochs 200
 
 # Train a model with CustomLoss on CIFAR100 dataset
-python main.py --loss CustomLoss --save_path ./results/CIFAR100/exp2/ --model_path ./results/CIFAR100/exp1/checkpoint_8000.pth --dataset CIFAR100 --resume --max_num_epochs 200
+python main.py --loss CustomLoss --save_path ./results/CIFAR100/exp2/ --model_path ./results/CIFAR100/exp1/checkpoint_1000.pth --dataset CIFAR100 --resume --max_num_epochs 200
